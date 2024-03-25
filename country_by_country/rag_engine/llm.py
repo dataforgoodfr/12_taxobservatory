d16f8bcfdab5025af3fe3bf8806f2681b01cb40a
Alexis Cunin <alexis.cunin@ekimetrics.com>
2024-03-25 11:57:42 +0100

    add langchain rag first version

--- country_by_country/rag_engine/llm.py
+++ country_by_country/rag_engine/llm.py
@@ -1,15 +1,17 @@
 import os
+
 from dotenv import load_dotenv
-from langchain.llms import HuggingFaceHub
+from langchain_community.llms import HuggingFaceEndpoint
 
 load_dotenv()
 
+
 def get_llm(max_tokens=1024, temperature=0.0, verbose=True):
-    huggingfacehub_api_token = os.getenv("HUGGINGFACE_API_TOKEN")
-    llm = HuggingFaceHub(
-        repo_id="mistralai/Mistral-7B-v0.1",
-        huggingfacehub_api_token=huggingfacehub_api_token,
-        # model_kwargs={"temperature": 0.1, "max_new_tokens": 200},
-        # verbose=True
+    HUGGINGFACEHUB_API_TOKEN = os.getenv("HUGGINGFACE_ALEXIS_API_TOKEN")
+    llm = HuggingFaceEndpoint(
+        repo_id="mistralai/Mistral-7B-v0.1",  # "mistralai/Mistral-7B-Instruct-v0.2"
+        huggingfacehub_api_token=HUGGINGFACEHUB_API_TOKEN,
+        # max_tokens=128,
+        # temperature=0.5,
     )
     return llm
