e5bb729c6137bf73eec616db14d269cab88fa32b
Guillaume Millot <guillaume@Guillaumes-MBP.lan>
2024-03-26 15:16:47 +0100

    Fix precommit

--- country_by_country/table_extraction/unstructured_api.py
+++ country_by_country/table_extraction/unstructured_api.py
@@ -22,10 +22,10 @@
 
 # Standard imports
 import logging
-import uuid
 
 # External imports
 import os
+import uuid
 from io import StringIO
 
 import pandas as pd
@@ -50,26 +50,30 @@ class UnstructuredAPI:
         logging.info("\nKicking off extraction stage...")
         logging.info(f"Extraction type: {self.type}, with params: {self.kwargs}")
 
-        s = UnstructuredClient(api_key_auth=os.getenv('UNSTRUCTURED_API_KEY'))
+        s = UnstructuredClient(api_key_auth=os.getenv("UNSTRUCTURED_API_KEY"))
 
         with open(pdf_filepath, "rb") as f:
-        # Note that this currently only supports a single file
-            files=shared.Files(
-            content=f.read(),
-            file_name=pdf_filepath,
-	    )
+            # Note that this currently only supports a single file
+            files = shared.Files(
+                content=f.read(),
+                file_name=pdf_filepath,
+            )
 
         req = shared.PartitionParameters(
             files=files,
-            strategy='hi_res',
-            pdf_infer_table_structure='True',
+            strategy="hi_res",
+            pdf_infer_table_structure="True",
             **self.kwargs,
         )
 
         try:
             resp = s.general.partition(req)
 
-            tables_list = [pd.read_html(StringIO(el["metadata"]["text_as_html"]))[0] for el in resp.elements if el["type"] == "Table"]
+            tables_list = [
+                pd.read_html(StringIO(el["metadata"]["text_as_html"]))[0]
+                for el in resp.elements
+                if el["type"] == "Table"
+            ]
 
             # Create asset
             new_asset = {
@@ -80,6 +84,6 @@ class UnstructuredAPI:
             }
 
             return new_asset
-        
+
         except SDKError as e:
-           print(e)
+            print(e)
