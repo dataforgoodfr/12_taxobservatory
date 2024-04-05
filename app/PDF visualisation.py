4884e07acba87b97a7b14dcba575cb1b247086d2
Ronan <ronan_2@hotmail.com>
2024-04-05 23:09:23 +0200

    feat: use pages to separate the different part of the tool

---
+++ app/PDF visualisation.py
@@ -0,0 +1,96 @@
+import logging
+import sys
+import tempfile
+
+import streamlit as st
+import yaml
+from utils import get_pdf_iframe
+
+from country_by_country.processor import ReportProcessor
+from country_by_country.utils.utils import keep_pages
+
+logging.basicConfig(stream=sys.stdout, level=logging.INFO, format="%(message)s")
+
+st.set_page_config(layout="wide")
+st.title("Country by Country Tax Reporting analysis")
+st.subheader(
+    "This app will help you extract a table containing financial information from a pdf",
+)
+
+mytmpfile = tempfile.NamedTemporaryFile(suffix=".pdf", delete=False)
+
+with st.sidebar:
+    pdf = st.file_uploader("Upload a pdf document containing financial table : ")
+
+    if pdf is not None:
+        mytmpfile.write(pdf.read())
+        st.session_state["original_pdf"] = mytmpfile
+    if "original_pdf" in st.session_state:
+        st.markdown("Already loaded file : " + st.session_state["original_pdf"].name)
+
+    loaded_config = st.file_uploader(
+        "Upload a config if the default config doesn't suit you :",
+    )
+
+    yaml_config = """
+pagefilter:
+  type: RFClassifier
+  params:
+    modelfile: random_forest_model_low_false_positive.joblib
+
+table_extraction:
+  - type: Camelot
+    params:
+      flavor: stream
+  - type: Camelot
+    params:
+      flavor: lattice
+  - type: Unstructured
+    params:
+      hi_res_model_name: "yolox"
+      pdf_image_dpi: 300
+    """
+
+    if loaded_config is None:
+        st.session_state["config"] = yaml.safe_load(yaml_config)
+    else:
+        st.session_state["config"] = yaml.safe_load(loaded_config)
+    yaml_str = yaml.dump(st.session_state["config"], default_flow_style=False, indent=2)
+    # Ajouter des backticks triples pour créer un bloc de code markdown
+    markdown_str = f"The configuration is : \n\n```\n{yaml_str}\n```"
+    st.write(markdown_str)
+
+if "original_pdf" in st.session_state:
+
+    st.markdown(
+        get_pdf_iframe(st.session_state["original_pdf"].name),
+        unsafe_allow_html=True,
+    )
+
+    if "first_time" not in st.session_state:
+        st.session_state["first_time"] = False
+        logging.info("Loading config and pdf")
+        st.session_state["proc"] = ReportProcessor(st.session_state["config"])
+
+        logging.info("Config and pdf loaded")
+
+        assets = {
+            "pagefilter": {},
+            "table_extractors": [],
+        }
+
+        # Filtering the pages
+        st.session_state["proc"].page_filter(
+            st.session_state["original_pdf"].name,
+            assets,
+        )
+
+        logging.info(f"Assets : {assets}")
+
+        st.session_state["pdf_before_page_validation"] = keep_pages(
+            st.session_state["original_pdf"].name,
+            assets["pagefilter"]["selected_pages"],
+        )
+        assets["pagefilter"]["selected_pages"] = []
+        st.session_state["assets"] = assets
+        st.switch_page("pages/1_Selected_Pages.py")
---
+++ app/pages/1_Selected_Pages.py
@@ -0,0 +1,67 @@
+import streamlit as st
+from utils import get_pdf_iframe, set_validate
+from country_by_country.utils.utils import keep_pages
+from pypdf import PdfReader
+
+import sys
+import logging
+
+logging.basicConfig(stream=sys.stdout, level=logging.INFO, format="%(message)s")
+
+st.set_page_config(layout="wide")  # page_icon="📈"
+st.title("Country by Country Tax Reporting analysis : Selected Pages")
+st.subheader(
+    "This page will allow you to select the pages containing your tables",
+)
+
+if "original_pdf" in st.session_state:
+
+    col1, col2 = st.columns([1, 1])
+    with col1:
+        st.markdown(
+            get_pdf_iframe(st.session_state["pdf_before_page_validation"]),
+            unsafe_allow_html=True,
+        )
+
+    print("SELECTED PAGES :")
+    print(st.session_state["assets"]["pagefilter"]["selected_pages"])
+
+    with col2, st.form(key="selected_pages_form"):
+        pdfreader = PdfReader(st.session_state["original_pdf"])
+        number_pages = (
+            len(PdfReader(st.session_state["pdf_before_page_validation"]).pages) + 1
+        )  # Make the index start to 1
+        st.session_state["assets"]["pagefilter"]["selected_pages"] = st.multiselect(
+            "Which page of the following pdf contains the table you want to extract ?",
+            list(range(1, number_pages)),
+            placeholder="Select a page number",
+            default=(
+                st.session_state["assets"]["pagefilter"]["selected_pages"]
+                if "assets" in st.session_state
+                else None
+            ),
+        )
+        # TODO : add a new button in order to use original pdf if the table was not found
+        submitted = st.form_submit_button(
+            label="Validate your selected pages",
+            on_click=set_validate,
+            args=("validate_selected_pages",),
+        )
+
+    print("SELECTED PAGES AFTER:")
+    print(st.session_state["assets"]["pagefilter"]["selected_pages"])
+
+    st.session_state["pdf_after_page_validation"] = keep_pages(
+        st.session_state["pdf_before_page_validation"],
+        [
+            item - 1
+            for item in st.session_state["assets"]["pagefilter"]["selected_pages"]
+        ],
+    )
+
+    if (
+        st.session_state["assets"]["pagefilter"]["selected_pages"] != []
+        and "first_time_selected" not in st.session_state
+    ):
+        st.session_state["first_time_selected"] = False
+        st.switch_page("pages/2_Headers.py")
