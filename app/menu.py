b3036d240e822b339537dc81fd31334145d58830
Quentin Jarry <quent1_jarry@hotmail.com>
2024-04-09 11:13:02 +0200

    add index as app entrypoint

--- app/menu.py
+++ app/menu.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 
 def display_pages_menu() -> None:
-    st.sidebar.page_link("index.py", label="Home - upload PDF")
+    st.sidebar.page_link("pages/0_Import_File.py", label="Upload PDF")
     st.sidebar.page_link("pages/1_Selected_Pages.py", label="Pages selection")
     st.sidebar.page_link("pages/2_Merge_Tables.py", label="Merge tables")
     st.sidebar.page_link("pages/3_Clean_Headers.py", label="Headers setup")
