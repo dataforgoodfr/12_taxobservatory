13b9d0033ab14f09f611a4e850152f516ea140d8
Ronan <ronan_2@hotmail.com>
2024-04-09 23:04:50 +0200

    feat: clean up old code

--- app/pages/4_Clean_Tables.py
+++ app/pages/4_Clean_Tables.py
@@ -1,5 +1,4 @@
 import streamlit as st
-from st_aggrid import AgGrid, GridOptionsBuilder, JsCode
 from utils import set_algorithm_name, get_pdf_iframe
 
 import sys
@@ -8,18 +7,6 @@ import pandas as pd
 import numpy as np
 
 logging.basicConfig(stream=sys.stdout, level=logging.INFO, format="%(message)s")
-
-
-def apply_filter(column_name: str, algorithm_name: str) -> None:
-
-    if st.session_state[column_name + algorithm_name] is not None:
-        st.session_state["filters_selected" + "_" + algorithm_name][
-            column_name
-        ] = st.session_state[column_name + algorithm_name]
-        # update_gridoption_cellstyle(column_name, js_code, algorithm_name)
-    else:
-        del st.session_state["filters_selected" + "_" + algorithm_name][column_name]
-        # remove_gridoption_cellstyle(column_name, algorithm_name)
 
 
 def check_last_cell_sum(column):
@@ -107,79 +94,43 @@ if (
             height=900,
         )
 
-    if (
-        "filters_selected" + "_" + st.session_state["algorithm_name"]
-        not in st.session_state
-    ):
-        st.session_state[
-            "filters_selected" + "_" + st.session_state["algorithm_name"]
-        ] = {}
+    col7, col8, col9 = st.columns([1, 1, 1])
+    with col7:
+        total = st.checkbox(
+            "Calculate the Total of each columns, excluding the last row"
+        )
+    with col8:
+        negativ = st.checkbox(
+            "Show the negative numbers, for each columns detected as a numerical type"
+        )
+    with col9:
+        symbol = st.checkbox(
+            "Show the cells that contain a special symbol : " + special_characters
+        )
 
-    col5, col6 = st.columns([1, 3])
-    filter_list = ["is_number", "is_negative"]
-    with col5:
-        for column_name in list(edited_df.columns.values):
-            if (
-                column_name
-                in st.session_state[
-                    "filters_selected" + "_" + st.session_state["algorithm_name"]
-                ]
-            ):
-                index = filter_list.index(
-                    st.session_state[
-                        "filters_selected" + "_" + st.session_state["algorithm_name"]
-                    ][column_name],
-                )
-            else:
-                index = None
-            st.selectbox(
-                "Do you want to apply a filter to the column " + column_name,
-                filter_list,
-                index=index,
-                placeholder="Select a filter",
-                on_change=apply_filter,
-                key=column_name + st.session_state["algorithm_name"],
-                args=(column_name, st.session_state["algorithm_name"]),
+    if total or negativ or symbol:
+        dataframe = edited_df
+        new_row = edited_df.apply(column_sum, axis=0)
+        new_row.iloc[0] = "Total Calculated"
+        dataframe.loc[-1] = new_row.transpose()
+        dataframe_styler = dataframe.style
+        if total:
+            dataframe_styler = dataframe_styler.apply(
+                check_last_cell_sum,
+                subset=pd.IndexSlice[:, dataframe.columns[1:]],
+                axis=0,
             )
-
-    with col6:
-        col7, col8, col9 = st.columns([1, 1, 1])
-        with col7:
-            total = st.checkbox(
-                "Calculate the Total of each columns, excluding the last row"
+        if negativ:
+            numeric_columns = edited_df.select_dtypes(include=np.number).columns
+            dataframe_styler = dataframe_styler.map(
+                style_negative,
+                props="color:red;",
             )
-        with col8:
-            negativ = st.checkbox(
-                "Show the negative numbers, for each columns detected as a numerical type"
+        if symbol:
+            dataframe_styler = dataframe_styler.map(
+                style_symbol,
+                props="color:red;",
             )
-        with col9:
-            symbol = st.checkbox(
-                "Show the cells that contain a special symbol : " + special_characters
-            )
-
-        if total or negativ or symbol:
-            dataframe = edited_df
-            new_row = edited_df.apply(column_sum, axis=0)
-            new_row.iloc[0] = "Total Calculated"
-            dataframe.loc[-1] = new_row.transpose()
-            dataframe_styler = dataframe.style
-            if total:
-                dataframe_styler = dataframe_styler.apply(
-                    check_last_cell_sum,
-                    subset=pd.IndexSlice[:, dataframe.columns[1:]],
-                    axis=0,
-                )
-            if negativ:
-                numeric_columns = edited_df.select_dtypes(include=np.number).columns
-                dataframe_styler = dataframe_styler.map(
-                    style_negative,
-                    props="color:red;",
-                )
-            if symbol:
-                dataframe_styler = dataframe_styler.map(
-                    style_symbol,
-                    props="color:red;",
-                )
-            st.dataframe(dataframe_styler, use_container_width=True, height=1000)
-        else:
-            st.dataframe(edited_df, use_container_width=True, height=1000)
+        st.dataframe(dataframe_styler, use_container_width=True, height=1000)
+    else:
+        st.dataframe(edited_df, use_container_width=True, height=1000)
