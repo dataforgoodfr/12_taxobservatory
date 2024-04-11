074ec2a213609ca9b4ddff7543c8aeb907d2fc59
Quentin Jarry <quent1_jarry@hotmail.com>
2024-04-11 16:09:52 +0200

    fix pdf_iframe width

--- app/utils.py
+++ app/utils.py
@@ -8,7 +8,7 @@ def get_pdf_iframe(pdf_to_process: str) -> str:
     base64_pdf = base64.b64encode(Path(pdf_to_process).read_bytes()).decode("utf-8")
     pdf_display = f"""
     <iframe src="data:application/pdf;base64,{base64_pdf}
-    " width="800px" height="1000px" type="application/pdf"></iframe>
+    " width="100%" height="1000px" type="application/pdf"></iframe>
     """
     return pdf_display
 
