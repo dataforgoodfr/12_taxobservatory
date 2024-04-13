dfe10dd91b88030e3770b4bd1f1117cad2e6d564
Jeremy Fix <jeremy.fix@centralesupelec.fr>
2024-04-13 15:40:21 +0200

    adds the name of the PDF processed in the sidebar

--- app/menu.py
+++ app/menu.py
@@ -2,6 +2,12 @@ import streamlit as st
 
 
 def display_pages_menu() -> None:
+
+    with st.sidebar:
+        if "original_pdf_name" in st.session_state:
+            st.markdown(
+                f"Currently processing : {st.session_state['original_pdf_name']}"
+            )
     st.sidebar.page_link("pages/0_Import_File.py", label="Upload PDF")
     st.sidebar.page_link("pages/1_Selected_Pages.py", label="Pages selection")
     st.sidebar.page_link("pages/2_Merge_Tables.py", label="Merge tables")
