8d676436177d946305e9f22943a0d38fca4013a1
Florent Scarpa <florent.scarpa@hotmail.fr>
2024-04-18 15:33:42 +0200

    Fix failing test

--- collecte/pdf_downloader.py
+++ collecte/pdf_downloader.py
@@ -73,6 +73,7 @@ def download_pdf(
 
     Path.mkdir(company_folder, parents=True, exist_ok=True)
     local_filename = Path(company_folder) / url.split("/")[-1]
+    exception_status = None
 
     if not Path.exists(local_filename):
 
@@ -131,8 +132,6 @@ def download_pdf(
                             f"PDF file '{local_filename}' seems broken and will be discarded",
                         )
                         local_filename.unlink(missing_ok=True)
-                    else:
-                        exception_status = None
 
         except requests.RequestException as e:
             logger.logger.exception(f"Failed to download {url}")
@@ -142,7 +141,6 @@ def download_pdf(
         logger.logger.warning(
             f"File '{local_filename}' already exists, download ignored",
         )
-        exception_status = None
 
     return exception_status
 
