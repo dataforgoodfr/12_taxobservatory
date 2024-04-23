4215d3f3cc11c460f17d15e97caf3d867cb74ef0
Quentin Jarry <quent1_jarry@hotmail.com>
2024-04-23 19:45:02 +0200

    feat multiselect for parser + radio button for page detection

--- app/pages/0_Import_File.py
+++ app/pages/0_Import_File.py
@@ -15,6 +15,9 @@ logging.basicConfig(stream=sys.stdout, level=logging.INFO, format="%(message)s")
 def set_extractors(value: list):
     set_state(["config", "table_extraction"], value)
 
+def set_page_filter(value: dict):
+    set_state(["config", "pagefilter"], value)
+
 st.set_page_config(layout="wide", page_title="Accueil - upload de PDF")
 st.title("Country by Country Tax Reporting analysis")
 st.subheader(
@@ -26,7 +29,7 @@ mytmpfile = tempfile.NamedTemporaryFile(suffix=".pdf", delete=False)
 
 with st.sidebar:
 
-    st.markdown("# Configuration")
+    st.markdown("# PDF Upload")
 
     original_pdf = st.file_uploader(
         "Upload a pdf document containing financial table : ",
@@ -42,25 +45,32 @@ with st.sidebar:
             "Already loaded file : " + st.session_state["original_pdf_name"],
         )
 
+    st.markdown("# Configuration:\n")
+    #Upload personalized config if required
     loaded_config = st.file_uploader(
         "Upload a config if the default config doesn't suit you :",
     )
 
+    #Default extract config
     with open("app/extract_config.yaml", "r") as f:
         default_config=f.read()
 
-
-    if loaded_config is None:
-        config = yaml.safe_load(default_config)        
-    else:
+    if bool(loaded_config):
         config = yaml.safe_load(loaded_config)
-    
-    if st.session_state.get("first_time", True):
-        st.session_state["config"] = config
+    else:
+        config = yaml.safe_load(default_config)
 
-    st.write("Config:\n\n")
-    page_filter_list = [pagefilter["type"] for pagefilter in config["pagefilter"]]
-    st.radio("Page filter", page_filter_list)
+    if st.session_state.get("first_time", True):
+        st.session_state["config"] = config 
+ 
+        
+    
+    
+
+    #Set page filter
+    page_filter_radio_dict = {pagefilter["type"]: pagefilter for pagefilter in config["pagefilter"]}
+    selected_page_filter = st.radio("Page filter", page_filter_radio_dict.keys())
+    set_page_filter(page_filter_radio_dict[selected_page_filter])
     
     #Set extractors
     all_table_extractors = {extractor["type"]: extractor for extractor in config["table_extraction"]}
@@ -72,6 +82,35 @@ with st.sidebar:
     # Ajouter des backticks triples pour créer un bloc de code markdown
     markdown_str = f"The configuration is : \n\n```\n{yaml_str}\n```"
     st.write(markdown_str)
+
+    if "working_file_pdf" in st.session_state:
+        if st.button("Extract tables", type="primary", use_container_width=True):
+            logging.info("Loading config and pdf")
+            st.session_state["proc"] = ReportProcessor(st.session_state["config"])
+
+            logging.info("Config and pdf loaded")
+
+            assets = {
+                "pagefilter": {},
+                "table_extractors": [],
+            }
+
+            # Filtering the pages
+            st.session_state["proc"].page_filter(
+                st.session_state["working_file_pdf"].name,
+                assets,
+            )
+
+            logging.info(f"Assets : {assets}")
+
+            if len(assets["pagefilter"]["selected_pages"]) == 0:
+                # No page has been automatically selected by the page filter
+                # Hence, we display the full pdf, letting the user select the pages
+                pdfreader = PdfReader(st.session_state["working_file_pdf"])
+                number_pages = len(PdfReader(st.session_state["working_file_pdf"]).pages)
+                assets["pagefilter"]["selected_pages"] = list(range(number_pages))
+            st.session_state["assets"] = assets
+            st.switch_page("pages/1_Selected_Pages.py")
 
 if "working_file_pdf" in st.session_state:
     # Once a pdf has been uploaded, it will be stored as
@@ -85,33 +124,7 @@ if "working_file_pdf" in st.session_state:
     )
 
 
-
     if "first_time" not in st.session_state:
         st.session_state["first_time"] = False
-        logging.info("Loading config and pdf")
-        st.session_state["proc"] = ReportProcessor(st.session_state["config"])
-
-        logging.info("Config and pdf loaded")
-
-        assets = {
-            "pagefilter": {},
-            "table_extractors": [],
-        }
-
-        # Filtering the pages
-        st.session_state["proc"].page_filter(
-            st.session_state["working_file_pdf"].name,
-            assets,
-        )
-
-        logging.info(f"Assets : {assets}")
-
-        if len(assets["pagefilter"]["selected_pages"]) == 0:
-            # No page has been automatically selected by the page filter
-            # Hence, we display the full pdf, letting the user select the pages
-            pdfreader = PdfReader(st.session_state["working_file_pdf"])
-            number_pages = len(PdfReader(st.session_state["working_file_pdf"]).pages)
-            assets["pagefilter"]["selected_pages"] = list(range(number_pages))
-        st.session_state["assets"] = assets
-        st.switch_page("pages/1_Selected_Pages.py")
+        
 
