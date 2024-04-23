c1c9e53a115da7755d0fed612b042dcee6729e9d
Quentin Jarry <quent1_jarry@hotmail.com>
2024-04-23 14:29:57 +0200

    add set_state utils function

--- app/utils.py
+++ app/utils.py
@@ -21,3 +21,22 @@ def set_algorithm_name(my_key: str) -> None:
 @st.cache_data
 def to_csv_file(df: pd.DataFrame) -> bytes:
     return df.to_csv(index=False).encode("utf-8")
+
+def set_state(key, value):
+    """
+        Sets the session_state[key] to value.
+        key can be a list to reach nested values. Ex: ["key1", "key2"] to reach session_state["key1"]["key2"] value.
+    """
+    if isinstance(key, list):
+        key_list = key
+        nested_key_string ="session_state"
+        nested_value = st.session_state
+        for k in key_list[:-1]:
+            try:
+                nested_key_string += f"['{k}']" 
+                nested_value = nested_value[k]
+            except KeyError:
+                raise KeyError(f"{nested_key_string} does not exist")
+        nested_value[key_list[-1]] = value
+    else: 
+        st.session_state[key] = value
