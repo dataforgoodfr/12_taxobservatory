a08afb68f5f8dfa89540133c9f912fbce4b432d9
Jeremy Fix <jeremy.fix@centralesupelec.fr>
2024-04-24 08:45:38 +0200

    add a case where the metadata step is skipped

--- app/utils.py
+++ app/utils.py
@@ -20,11 +20,22 @@ def set_algorithm_name(my_key: str) -> None:
 
 @st.cache_data
 def to_csv_file(df: pd.DataFrame) -> bytes:
-    # Populate the columns with the metadata
-    df = df.assign(company=st.session_state["metadata"]["company_name"])
-    df = df.assign(year=st.session_state["metadata"]["sector"])
-    df = df.assign(year=st.session_state["metadata"]["year"])
-    df = df.assign(currency=st.session_state["metadata"]["currency"])
-    df = df.assign(unit=st.session_state["metadata"]["unit"])
-    df = df.assign(headquarter=st.session_state["metadata"]["headquarter"])
+    # Populate the columns with the metadata, if available
+    # They may not be available if the user skipped the metadata page
+    # by not clicking on Submit
+    if "metadata" in st.session_state:
+        df = df.assign(company=st.session_state["metadata"]["company_name"])
+        df = df.assign(sector=st.session_state["metadata"]["sector"])
+        df = df.assign(year=st.session_state["metadata"]["year"])
+        df = df.assign(currency=st.session_state["metadata"]["currency"])
+        df = df.assign(unit=st.session_state["metadata"]["unit"])
+        df = df.assign(headquarter=st.session_state["metadata"]["headquarter"])
+    else:
+        df = df.assign(company="")
+        df = df.assign(sector="")
+        df = df.assign(year="")
+        df = df.assign(currency="")
+        df = df.assign(unit="")
+        df = df.assign(headquarter="")
+
     return df.to_csv(index=False).encode("utf-8")
